f6ea58e055dda2503a9671954fcd3031921ee178
Aloisio Spadeto <aspadeto@gmail.com>
2019-06-23 20:43:49 -0400

    Ferramenta para desabilitar a hibernação

---
+++ tools/DisableHibernation.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Obtido em https://www.tonymacx86.com/threads/guide-native-power-management-for-laptops.175801/
+# 
+#
+#
+#
+
+
+sudo pmset -a hibernatemode 0
+sudo rm -f /var/vm/sleepimage
+sudo mkdir /var/vm/sleepimage # try to prevent update from re-enabling
+sudo pmset -a standby 0
+sudo pmset -a autopoweroff 0
